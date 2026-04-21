4addd52e8c25e35f7fe18de98cf93975a54b2666
runningpig66 <runningpig66@gmail.com>
2026-04-21 20:47:20 +0800

    Dessert Release - Save preferences locally with DataStore [main]

---
+++ Unit6/DessertRelease/app/src/main/java/com/example/dessertrelease/DessertReleaseApplication.kt
@@ -0,0 +1,30 @@
+package com.example.dessertrelease
+
+import android.app.Application
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.example.dessertrelease.data.UserPreferencesRepository
+
+/**
+ * @author runningpig66
+ * @date 2026-04-21
+ * @time 18:18
+ */
+private const val LAYOUT_PREFERENCE_NAME = "layout_preferences"
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+    name = LAYOUT_PREFERENCE_NAME
+)
+
+/*
+ * Custom app entry point for manual dependency injection
+ */
+class DessertReleaseApplication : Application() {
+    lateinit var userPreferencesRepository: UserPreferencesRepository
+
+    override fun onCreate() {
+        super.onCreate()
+        userPreferencesRepository = UserPreferencesRepository(dataStore)
+    }
+}
--- Unit6/DessertRelease/app/src/main/java/com/example/dessertrelease/MainActivity.kt
+++ Unit6/DessertRelease/app/src/main/java/com/example/dessertrelease/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import com.example.dessertrelease.ui.DessertReleaseApp
 import com.example.dessertrelease.ui.theme.DessertReleaseTheme
 
+// Unit 6: Save preferences locally with DataStore
+// https://github.com/google-developer-training/basic-android-kotlin-compose-training-dessert-release/tree/main
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
